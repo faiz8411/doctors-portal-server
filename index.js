45a8a239bbe3182e463e2df0369203fe34d132c5
faiz8411 <faizullah.noa2014@gmail.com>
2021-11-10 00:33:58 +0600

    locally date change

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ async function run() {
         // get appointments
         app.get('/appointments', async (req, res) => {
             const email = req.query.email
-            const date = new Date(req.query.date).toLocaleDateString()
+            const date = req.query.date
             // console.log(date)
             const query = { email: email, date: date }
             const cursor = appointmentCollection.find(query)
